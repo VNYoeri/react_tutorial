6c7492487aba2d582a220dd45830076682661ceb
Yoeri Van Nieuwerburg <yoeri.van.nieuwerburg@tvh.com>
2024-10-10 18:42:23 +0200

    fix layout using bulma css

--- digital_assistant/src/ProfileCard.tsx
+++ digital_assistant/src/ProfileCard.tsx
@@ -1,20 +1,54 @@
-interface ProfileCardProps {
+import 'bulma/css/bulma.css';
+import React from "react";
+
+interface ProfileCard {
     title?: string
     handle?: string
     image?: string
+    description?: string
 }
 
-function ProfileCard({handle, image, title}: ProfileCardProps) {
-    const imageAlt=`PDA logo for ${title}`
+function ProfileCard({handle, image, title, description}: ProfileCard) {
+    const imageAlt = `PDA logo for ${title}`
     return (
-        <div>
-            <img src={image} alt={imageAlt}/>
-            <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-                <div>{title}</div>
-                <div>{handle}</div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt={imageAlt}/>
+                </figure>
+            </div>
+
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+                <div className="content">
+                    {description}
+                </div>
             </div>
         </div>
     );
+}
+
+// FIXME: using this approach, the image don't render. Probably needs some sort of setState, due to the async-nature of fetching images?
+interface ProfileCardsProps {
+    cards?: ProfileCard[]
+}
+
+function ProfileCards(cardsProps: ProfileCardsProps) {
+
+    return (<div className="container">
+        <section className="section">
+            <div className="columns">
+                {cardsProps.cards?.map(card => (
+                    <div className="column is-4" key={card.title}>
+                        <ProfileCard title={card.title} handle={card.handle} image={card.image}/>
+                    </div>
+                ))}
+            </div>
+        </section>
+    </div>)
 }
 
 export default ProfileCard
