cc745a3bcb9ae28baa0a8be103200956329ce4a6
Yoeri Van Nieuwerburg <yoeri.van.nieuwerburg@tvh.com>
2024-10-14 08:58:25 +0200

    use State

--- animals/src/App.tsx
+++ animals/src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import Animal from './Animal';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-      <Animal />
-  );
+    const [count, setCount] = useState(0)
+
+    const handleClick = () => {
+        setCount(count + 1)
+    };
+    return (
+        <div>
+            <button onClick={handleClick}>Add Animal</button>
+            <div>Number of animals is: {count}</div>
+        </div>
+    );
 }
 
 export default App;
