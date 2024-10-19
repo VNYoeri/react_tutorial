eced90d69a904910afc1ec2909930cc974357082
Yoeri Van Nieuwerburg <yoeri.van.nieuwerburg@tvh.com>
2024-10-19 13:51:57 +0200

    finish styling and use ...rest operator combined with index signatures (ts)

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,16 +1,24 @@
 import Button, {ButtonType} from "./button";
+import React from "react";
+import {GoBell, GoDatabase, GoDownload} from "react-icons/go";
 
 function App() {
     return (
         <div>
             <div>
-                <Button type={ButtonType.PRIMARY}>PRIMARY - not rounded - filled</Button>
+                <Button type={ButtonType.PRIMARY}>
+                    <GoBell /> PRIMARY - not rounded - filled
+                </Button>
             </div>
             <div>
-                <Button outline type={ButtonType.SECONDARY}>SECONDARY - not rounded - outlined</Button>
+                <Button outline type={ButtonType.SECONDARY}>
+                    <GoDownload /> SECONDARY - not rounded - outlined
+                </Button>
             </div>
             <div>
-                <Button rounded type={ButtonType.DANGER}>DANGER - rounded - not outlined</Button>
+                <Button rounded type={ButtonType.DANGER}>
+                    <GoDatabase /> DANGER - rounded - not outlined
+                </Button>
             </div>
             <div>
                 <Button rounded outline type={ButtonType.WARNING}>WARNING - rounded - outlined</Button>
--- components/src/button.tsx
+++ components/src/button.tsx
@@ -4,13 +4,13 @@ interface ButtonProps {
     rounded?: boolean,
     outline?: boolean,
     type: ButtonType,
-    children: ReactNode
+    children: ReactNode,
+    [x:string]:any
 }
 
-function Button({rounded, outline, type, children}: ButtonProps) {
+function Button({rounded, outline, type, children, ...rest}: ButtonProps) {
 
-
-    return <button className={`px-3 py-1.5 text-white ${buttonColorFor(type)}`}>
+     return <button {...rest} className={`${buttonStyleFor(type, rounded, outline)}`}>
         {children}
     </button>
 }
@@ -25,12 +25,75 @@ export const enum ButtonType {
     DANGER
 }
 
-function buttonColorFor(type: ButtonType): string {
-    switch(type) {
-        case ButtonType.SECONDARY:  return "border-black bg-gray-500"
-        case ButtonType.SUCCESS:  return "border-green-600 bg-green-500"
-        case ButtonType.WARNING:  return "border-red-600 bg-red-500"
-        case ButtonType.DANGER:  return "border-yellow-600 bg-yellow-500"
-        case ButtonType.PRIMARY: return "border-blue-600 bg-blue-500"
+function buttonStyleFor(type: ButtonType, rounded?: boolean, outlined?: boolean): string {
+    switch (type) {
+        case ButtonType.PRIMARY:
+            return new PrimaryButton(rounded, outlined).finalStyle()
+        case ButtonType.SECONDARY:
+            return new SecondaryButton(rounded, outlined).finalStyle()
+        case ButtonType.SUCCESS:
+            return new SuccessButton(rounded, outlined).finalStyle()
+        case ButtonType.DANGER:
+            return new DangerButton(rounded, outlined).finalStyle()
+        case ButtonType.WARNING:
+            return new WarningButton(rounded, outlined).finalStyle()
     }
+}
+
+abstract class ButtonClassStyle {
+    rounded;
+    outlined;
+    defaultStyle = 'flex items-center px-3 py-1.5 border'
+    textColor = 'text-white';
+    outlineBgStyle = 'bg-white';
+    abstract bgColor: string;
+    abstract outlineTextColor: string;
+    abstract borderColor: string;
+
+    constructor(rounded: boolean = false, outlined: boolean = false) {
+        this.rounded = rounded;
+        this.outlined = outlined;
+    }
+
+    borderRadius() {
+      return this.rounded ? 'rounded-full' : 'rounded-none';
+    }
+
+    finalStyle(): string {
+        if (this.outlined) {
+            return [this.defaultStyle, this.borderColor, this.outlineTextColor, this.outlineBgStyle, this.borderRadius()].join(' ')
+        }
+
+        return [this.defaultStyle, this.textColor, this.borderColor, this.bgColor, this.borderRadius()].join(' ')
+    }
+}
+
+class PrimaryButton extends ButtonClassStyle {
+    bgColor = 'bg-blue-500';
+    outlineTextColor='text-blue-500';
+    borderColor='border-blue-500';
+}
+
+class SecondaryButton extends ButtonClassStyle {
+    bgColor = 'bg-gray-900';
+    outlineTextColor='text-gray-900';
+    borderColor='border-gray-900';
+}
+
+class SuccessButton extends ButtonClassStyle {
+    bgColor = 'bg-green-500';
+    outlineTextColor = 'text-green-500';
+    borderColor='border-green-500';
+}
+
+class WarningButton extends ButtonClassStyle {
+    bgColor = 'bg-amber-400';
+    outlineTextColor='text-amber-400';
+    borderColor='border-amber-400';
+}
+
+class DangerButton extends ButtonClassStyle {
+    bgColor = 'bg-red-500';
+    outlineTextColor='text-red-500';
+    borderColor='border-red-500';
 }
