5adbb52d903c5471c14cc8a81b54dba1349e03e5
Yoeri Van Nieuwerburg <yoeri.van.nieuwerburg@tvh.com>
2024-11-05 16:31:21 +0100

    introduce reducers

--- components/src/pages/CounterPage.tsx
+++ components/src/pages/CounterPage.tsx
@@ -1,41 +1,68 @@
 import Button, {ButtonType} from '../components/Button';
-import {ChangeEvent, FormEvent, FormEventHandler, useState} from 'react';
+import {ChangeEvent, FormEvent, Reducer, useReducer} from 'react';
 import Panel from '../components/Panel';
 
 interface CounterProps {
     initialCount: number
 }
+enum ActionType {
+    INCREMENT_COUNT,
+    DECREMENT_COUNT,
+    UPDATE_VALUE_TO_ADD
+}
+
+interface Dispatch {
+    type: ActionType,
+    payload: number
+}
+
+
+interface CounterState {
+    count: number,
+    valueToAdd: number
+}
+
+const reducer = (state: CounterState, action: Dispatch) => {
+
+    switch (action.type) {
+        case ActionType.INCREMENT_COUNT: return {...state, count: state.count + action.payload}
+        case ActionType.DECREMENT_COUNT: return {...state, count: state.count - action.payload}
+        case ActionType.UPDATE_VALUE_TO_ADD: return {...state, valueToAdd: action.payload}
+    }
+};
 
 function CounterPage({initialCount}: CounterProps) {
-    const [count, setCount] = useState(initialCount);
-    const [valueToAdd, setValueToAdd] = useState(0);
+    const [state, dispatch] = useReducer<Reducer<CounterState, Dispatch>>(reducer, {
+        count: initialCount,
+        valueToAdd: 0
+    })
 
     const increment = () => {
-        setCount(count + 1);
+        dispatch({type: ActionType.INCREMENT_COUNT, payload: 1})
     }
     const decrement = () => {
-        setCount(count - 1);
+        dispatch({type: ActionType.DECREMENT_COUNT, payload: 1})
     }
 
     const handleForm = (event: FormEvent) => {
         event.preventDefault();
-        setCount(count + valueToAdd)
-        setValueToAdd(0)
+        dispatch({type: ActionType.INCREMENT_COUNT, payload: state.valueToAdd})
+        dispatch({type: ActionType.UPDATE_VALUE_TO_ADD, payload: 0})
     }
 
     const inputChanged = (event: ChangeEvent<HTMLInputElement>) => {
-        setValueToAdd(event.target.valueAsNumber)
+        dispatch({type: ActionType.UPDATE_VALUE_TO_ADD, payload: event.target.valueAsNumber})
     }
 
     return (<Panel className={'m-3'}>
-            <h1 className={'text-lg'}>Count is {count}</h1>
+            <h1 className={'text-lg'}>Count is {state.count}</h1>
             <div className={'flex flex-row'}>
                 <Button onClick={increment} type={ButtonType.PRIMARY}>Increment</Button>
                 <Button onClick={decrement} type={ButtonType.SECONDARY}>Decrement</Button>
             </div>
             <form onSubmit={handleForm}>
                 <label>Add a lot!!</label>
-                <input value={valueToAdd} onInput={inputChanged} type="number" className={'p-1 m-3 bg-gray-50 border border-gray-300'}/>
+                <input value={state.valueToAdd} onInput={inputChanged} type="number" className={'p-1 m-3 bg-gray-50 border border-gray-300'}/>
                 <Button type={ButtonType.PRIMARY} rounded outline>Add it!</Button>
             </form>
         </Panel>
