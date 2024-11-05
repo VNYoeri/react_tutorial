fee3b50c2e74474a311c817c3533c6ab62aa4b36
Yoeri Van Nieuwerburg <yoeri.van.nieuwerburg@tvh.com>
2024-11-05 11:55:39 +0100

    setup counterpage to introduce useReducer

--- components/src/pages/CounterPage.tsx
+++ components/src/pages/CounterPage.tsx
@@ -1,17 +1,44 @@
 import Button, {ButtonType} from '../components/Button';
-import useCounter from '../hooks/useCounter';
+import {ChangeEvent, FormEvent, FormEventHandler, useState} from 'react';
+import Panel from '../components/Panel';
 
 interface CounterProps {
     initialCount: number
 }
 
 function CounterPage({initialCount}: CounterProps) {
-    const {count, increment} = useCounter(initialCount);
+    const [count, setCount] = useState(initialCount);
+    const [valueToAdd, setValueToAdd] = useState(0);
 
-    return (<div>
-            <h1>Count is {count}</h1>
-            <Button onClick={increment} type={ButtonType.PRIMARY}>Increment</Button>
-        </div>
+    const increment = () => {
+        setCount(count + 1);
+    }
+    const decrement = () => {
+        setCount(count - 1);
+    }
+
+    const handleForm = (event: FormEvent) => {
+        event.preventDefault();
+        setCount(count + valueToAdd)
+        setValueToAdd(0)
+    }
+
+    const inputChanged = (event: ChangeEvent<HTMLInputElement>) => {
+        setValueToAdd(event.target.valueAsNumber)
+    }
+
+    return (<Panel className={'m-3'}>
+            <h1 className={'text-lg'}>Count is {count}</h1>
+            <div className={'flex flex-row'}>
+                <Button onClick={increment} type={ButtonType.PRIMARY}>Increment</Button>
+                <Button onClick={decrement} type={ButtonType.SECONDARY}>Decrement</Button>
+            </div>
+            <form onSubmit={handleForm}>
+                <label>Add a lot!!</label>
+                <input value={valueToAdd} onInput={inputChanged} type="number" className={'p-1 m-3 bg-gray-50 border border-gray-300'}/>
+                <Button type={ButtonType.PRIMARY} rounded outline>Add it!</Button>
+            </form>
+        </Panel>
     )
 
 }
