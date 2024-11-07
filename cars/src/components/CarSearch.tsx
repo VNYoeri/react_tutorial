7f684a9ab607a806eae7abdf38b6bf5014e4b2cf
Yoeri Van Nieuwerburg <yoeri.van.nieuwerburg@tvh.com>
2024-11-07 10:31:41 +0100

    search, filter and recalculate total

--- cars/src/components/CarSearch.tsx
+++ cars/src/components/CarSearch.tsx
@@ -1,5 +1,25 @@
+import {useDispatch, useSelector} from 'react-redux';
+import {AppDispatch, changeSearchTerm, RootState} from '../store';
+import {ChangeEvent} from 'react';
+
 function CarSearch() {
-    return <div>Search</div>
+    const dispatch: AppDispatch = useDispatch();
+    const searchTerm = useSelector((state: RootState) => {
+        return state.cars.searchTerm;
+    })
+
+    const search = (event: ChangeEvent<HTMLInputElement>) => {
+        dispatch(changeSearchTerm(event.target.value))
+    }
+
+
+    return <div className='list-header'>
+        <h3 className='title is-3'>My Cars</h3>
+        <div className='search field is-horizontal'>
+            <label className='label'>Search</label>
+            <input type='text' value={searchTerm} onChange={search} className='input'/>
+        </div>
+    </div>
 }
 
 export default CarSearch;
--- cars/src/components/CarValue.tsx
+++ cars/src/components/CarValue.tsx
@@ -3,10 +3,12 @@ import {RootState} from '../store';
 
 function CarValue() {
     const carTotalCost: number = useSelector((state: RootState) => {
-        return state.cars.cars.map(car => car.cost).reduce((x, y) => x + y, 0);
+        return state.cars.cars.filter(car => car.name.includes(state.cars.searchTerm)).map(car => car.cost).reduce((x, y) => x + y, 0);
     })
 
-    return <div>Total value: &#x20AC; {carTotalCost}</div>
+    return <div className='car-value'>
+        Total value: &#x20AC; {carTotalCost}
+    </div>
 
 }
 
