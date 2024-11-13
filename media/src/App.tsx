8b2f5b38bd1b6870ffea804d14e60ccf28ddc995
Yoeri Van Nieuwerburg <yoeri.van.nieuwerburg@tvh.com>
2024-11-13 13:48:39 +0100

    initial setup

---
+++ media/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='flex justify-center'>
+        <div className='text-xl'>Title</div>
+    </div>
+  );
+}
+
+export default App;
